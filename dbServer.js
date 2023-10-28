16b671cdefcada10e43f92f5c069bcdd7c674009
Lavish <64689132+lavish440@users.noreply.github.com>
2023-10-28 19:31:16 +0530

    Added backend code for User Registration

--- dbServer.js
+++ dbServer.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 const app = express();
 
 const mysql = require("mysql");
@@ -10,6 +11,7 @@ const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_DATABASE = process.env.DB_DATABASE;
 const DB_PORT = process.env.DB_PORT;
+const PORT = process.env.PORT;
 
 const db = mysql.createPool({
   connectionLimit: 100,
@@ -23,4 +25,43 @@ const db = mysql.createPool({
 db.getConnection((err, connection) => {
   if (err) throw err;
   console.log("DB Connected Succesful: " + connection.threadId);
+});
+
+app.listen(PORT, () => console.log("Server started on port " + PORT));
+app.use(express.json());
+
+app.post("/createUser", async (req, res) => {
+  const user = req.body.name;
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+  db.getConnection(async (err, connection) => {
+    if (err) throw err;
+
+    const sqlSearch = "SELECT * FROM hackathon WHERE user = ?";
+    const search_query = mysql.format(sqlSearch, [user]);
+
+    const sqlInsert = "INSERT INTO hackathon VALUES (0,?,?)";
+    const insert_query = mysql.format(sqlInsert, [user, hashedPassword]);
+
+    await connection.query(search_query, async (err, result) => {
+      if (err) throw err;
+      console.log("------> Search Results");
+      console.log(result.length);
+
+      if (result.length != 0) {
+        connection.release();
+        console.log("------> User already exists");
+        res.sendStatus(409);
+      } else {
+        await connection.query(insert_query, (err, result) => {
+          connection.release();
+
+          if (err) throw err;
+          console.log("------> Created new user");
+          console.log(result.insertId);
+          res.sendStatus(201);
+        });
+      }
+    });
+  });
 });
