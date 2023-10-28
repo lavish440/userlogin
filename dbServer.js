c294ccf005f4486846b8c0c6e403b468a9ad1575
Lavish <64689132+lavish440@users.noreply.github.com>
2023-10-29 02:50:59 +0530

    Added the ability to parse URL-Encoded POST requests

--- dbServer.js
+++ dbServer.js
@@ -29,6 +29,7 @@ db.getConnection((err, connection) => {
 
 app.listen(PORT, () => console.log("Server started on port " + PORT));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.post("/createUser", async (req, res) => {
   const user = req.body.user;
