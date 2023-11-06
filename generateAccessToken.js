14d9789416db66b23f088283daa722bdc48463ea
Lavish <64689132+lavish440@users.noreply.github.com>
2023-11-06 16:50:26 +0530

    Fixed a typo

--- generateAccessToken.js
+++ generateAccessToken.js
@@ -15,7 +15,7 @@ function validateToken(req, res, next) {
 
     if (token == null) res.sendStatus(400).send("Token not present");
 
-    jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
       if (err) {
         res.status(403).send("Token is very invalid");
       } else {
